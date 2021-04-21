f672e3fdc748f0b7fd24f7301a1a28c7b58f8447
mahdi66-ger <58116389+mahdi66-ger@users.noreply.github.com>
2021-04-21 18:13:42 +0200

    DB Verbindung

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,6 +1,12 @@
 
 from flask import Flask, request, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from models import db, MovieModel, select_data
 import csv
+import sqlalchemy
+
+
 
 users = {'mahdi': '123',
          'julian': '456',
@@ -9,6 +15,13 @@ users = {'mahdi': '123',
 
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:Sani43226117@localhost:5432/movie_rating"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
+db.init_app(app)
+Migrate = Migrate(app, db)
 
 @app.route("/")
 
@@ -39,12 +52,17 @@ def register():
         movie_name = request.form.get('moviename','None')
         year = request.form.get('year','None')
         try:
-            file = open('register_list.csv','a')  # a means append / r means read and w means write
-            writer = csv.writer(file)
-            writer.writerow((movie_name, year))
-            file.close()
-        except:
-            return "failed"
+            # file = open('register_list.csv','a')  # a means append / r means read and w means write
+            # writer = csv.writer(file)
+            # writer.writerow((movie_name, year))
+            # file.close()
+            print(select_data())
+            new_movie = MovieModel(name= movie_name, year= year)
+            db.session.add(new_movie)
+            db.session.commit()
+
+        except Exception  as e:
+            return e
         return render_template("registered_success.html", title= "Register Successfull")
     elif request.method == "GET":
         return render_template("register_movies.html", titel = "Register new Movie")
@@ -53,7 +71,13 @@ def register():
 
 @app.route("/registered_movies", methods = ['GET'])
 def render_movie_list():
-    file = open('C:/flask_tutorial/register_list.csv','r')
-    reader = csv.reader(file)
-    movies = list(reader)
-    return render_template("registered.html", titel = "List of Movies", movies=movies)
+    # with open('C:/flask_tutorial/register_list.csv','r') as file:
+    #     reader = csv.reader(file)
+    #     movies = list(reader)
+    #     result = [movie for movie in movies if movie]
+    result = select_data()
+    return render_template("registered.html", titel = "List of Movies", movies=result)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
