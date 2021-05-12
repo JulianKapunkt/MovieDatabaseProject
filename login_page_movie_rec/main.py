ecb69c739916e2772ac9c3792da70d132bc8f23e
mahdi66-ger <58116389+mahdi66-ger@users.noreply.github.com>
2021-05-12 20:09:54 +0200

    add functions to get attributes from forms

--- login_page_movie_rec/main.py
+++ login_page_movie_rec/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 # from flask_wtf import FlaskForm
 # from wtforms import StringField, PasswordField, SubmitField
 # from wtforms.validators import DataRequired, Email, Length
@@ -20,21 +20,40 @@ def home():
     return render_template("login.html")
 
 
-@app.route('/login')
+@app.route('/login' , methods =['GET', 'POST'])
 def login():
     # form = Login_Form()
-    return render_template("login.html")
+    if request.method == "POST":
+        email = request.form.get("exampleInputEmail1")
+        password = request.form.get("exampleInputPassword1")
+        check = request.form.get("exampleCheck1")  # if checked, then the value is 'on' else 'null'
+        return {'email': email , 'pass' : password, 'check': check}
+    #return render_template("login.html")
 
-@app.route('/sign_up')
+@app.route('/sign_up', methods = ['GET','POST'])
 def sign_up():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('exampleInputEmail1')
+        password = request.form.get('exampleInputPassword1')
+        password_repeated = request.form.get('exampleInputPassword2')
+        if password == password_repeated:
+            return {'username': username , 'email': email, 'pass': password}
+        else:
+            return "Password does not match"
     # form = Login_Form()
-    return render_template("sign_up.html")
+    elif request.method == 'GET':
+        return render_template("sign_up.html")
 
 @app.route('/forgot_password')
 def forgot_password():
     # form = Login_Form()
     return render_template("forgot_password.html")
 
-
+@app.route('/recover_user', methods = ['GET','POST'])
+def recover_user():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        return email
 if __name__ == "__main__":
     app.run(debug=True)
