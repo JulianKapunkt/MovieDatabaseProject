bd7f78d0057685e11d40cfa5f1f1d7f4e3315c37
mahdi66-ger <58116389+mahdi66-ger@users.noreply.github.com>
2021-05-27 20:07:13 +0200

    Test DB

--- Test_DB/app.py
+++ Test_DB/app.py
@@ -2,7 +2,7 @@
 from flask import Flask, request, render_template, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from models import select_data
+from models_flask import select_data, db, databaseURI
 import csv
 import sqlalchemy
 
@@ -12,23 +12,22 @@ import sqlalchemy
 
 app = Flask(__name__)
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:Sani43226117@localhost:5432/movie_rating"
+app.config['SQLALCHEMY_DATABASE_URI'] = databaseURI
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # app.config['SQLALCHEMY_ECHO'] = True
 
-#db.init_app(app)
+db.init_app(app)
 #Migrate = Migrate(app, db)
 
 @app.route("/")
-
-
-
 def index():
     #name = request.args.get('var', 'World')
     #return render_template('index.html', name = name)
     result = select_data()
-    b_result = str("user: "+result[0][0])
-    return str(result[1])
+    print (len(result))
+    b_result = str("username: "+ result[0].username + " pass: " + result[0].clear_password + " email: " + result[0].email)
+    
+    return  b_result
 
 
 
